8added99704ee730fad4cc6d5d8c007824c598b3
deliolleh <jhhan2206@naver.com>
2022-02-09 11:39:47 +0900

    SWEA project change

--- SWEA/Example/6308 - inner func.py
+++
@@ -1,8 +0,0 @@
-name = input()
-age = int(input())
-
-def calc_year_hund(a):
-    hund = 2019 - a +100
-    return hund
-
-print("{0}(은)는 {1}년에 100세가 될 것입니다.".format(name, calc_year_hund(age)))
--- SWEA/Example/6311 - inner func.py
+++
@@ -1,9 +0,0 @@
-A = "ADCBBBBCABBCBDACBDCAACDDDCAABABDBCBCBDBDBDDABBAAAAAAADADBDBCBDABADCADC"
-sum = 0
-
-A_map = list(map(lambda x: ord('E') - ord(x), A))
-
-for i in range(len(A_map)):
-    sum += A_map[i]
-
-print(sum)
