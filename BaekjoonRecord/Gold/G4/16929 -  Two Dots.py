2c64fe0f8f6cd9699388a19a8a98f678862e69eb
Olleh <jhhan2206@naver.com>
2022-04-19 23:35:38 +0900

    before

--- BaekjoonRecord/Gold/G4/16929 -  Two Dots.py
+++ BaekjoonRecord/Gold/G4/16929 -  Two Dots.py
@@ -2,12 +2,16 @@ import sys
 
 
 def dfs(ny, nx, cnt):
+    # 그냥하면 처음 위치에서도 순회했다고 판단할 수 있어
+    # 이를 방지하기 위한 순회횟수 cnt 추가
+    # 사이클이 생기려면 4이상이여야하지만 3일때는 불가능하므로 2초과로 범위를 해도 문제 X
     if ny == i and nx == j and cnt > 2:
         print('Yes')
         quit()
     else:
         for d in range(4):
             nny, nnx = ny + dy[d], nx + dx[d]
+            # 범위 내 + 색 동일 + 미방문
             if 0 <= nny < N and 0 <= nnx < M and dots[nny][nnx] == dots[ny][nx] and not visited[nny][nnx]:
                 visited[nny][nnx] = 1
                 dfs(nny, nnx, cnt + 1)
@@ -22,6 +26,7 @@ N, M = map(int, input().split())
 dots = [list(sys.stdin.readline().rstrip()) for _ in range(N)]
 visited = [[0] * M for _ in range(N)]
 
+# 이동하면서 계속 dfs => 범위가 50까지라 가까스로 된듯
 for i in range(N):
     for j in range(M):
         dfs(i, j, 0)
--- BaekjoonRecord/Sliver/S1/7562 - 나이트의 이동.py
+++ BaekjoonRecord/Sliver/S1/7562 - 나이트의 이동.py
@@ -14,19 +14,24 @@ def bfs():
                 que.append([ny, nx, cnt + 1])
 
 
+# 나이트가 이동할 수 있는 좌표
 dy = [2, 1, -1, -2, -2, -1, 1, 2]
 dx = [1, 2, 2, 1, -1, -2, -2, -1]
 TC = int(input())
 
 for tc in range(TC):
+    # 체스판의 한 변의 길이
     L = int(input())
 
     chess = [[0] * L for _ in range(L)]
 
+    # 현재 나이트가 있는 칸
     sy, sx = map(int, input().split())
 
+    # 도착지
     ey, ex = map(int, input().split())
 
+    # 출발지 방문체크
     chess[sy][sx] = 1
     que = deque()
     que.append([sy, sx, 0])
